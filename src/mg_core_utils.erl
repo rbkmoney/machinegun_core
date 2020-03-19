882ef932cca7aa7fbbf0606fb3b91b819b5acca1
Toporkov Igor <i.toporkov@rbkmoney.com>
2020-03-19 12:58:14 +0300

    Ensure that files are loaded before calling function_exported (#3)
    * Ensure that files are being loaded before calling function_exported
    
    * Rename function, enrich error log message

--- src/mg_core_utils.erl
+++ src/mg_core_utils.erl
@@ -245,11 +245,22 @@ apply_mod_opts_if_defined(ModOpts, Function, Default) ->
 apply_mod_opts_if_defined(ModOpts, Function, Default, Args) ->
     {Mod, Arg} = separate_mod_opts(ModOpts),
     FunctionArgs = [Arg | Args],
+    ok = maybe_load_module(Mod),
     case erlang:function_exported(Mod, Function, length(FunctionArgs)) of
         true ->
             erlang:apply(Mod, Function, FunctionArgs);
         false ->
             Default
+    end.
+
+-spec maybe_load_module(module()) ->
+    ok.
+maybe_load_module(Mod) ->
+    case code:ensure_loaded(Mod) of
+        {module, Mod} ->
+            ok;
+        {error, Reason} ->
+            logger:warning("An error occured, while loading module ~p, reason: ~p", [Mod, Reason])
     end.
 
 -spec separate_mod_opts(mod_opts()) ->
